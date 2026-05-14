a21889db551a5d60808ee5df721a90b7ff693512
Webcreator192302 <ggibson2244@gmail.com>
2026-05-14 14:28:46 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { ownerNotificationEmailTemplate, confirmationEmailTemplate } from './emails/templates';
 import { useState, useEffect, useRef } from 'react';
 import { 
   Menu, 
